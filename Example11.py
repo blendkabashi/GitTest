e557166f1966047a0d6b2b8eda75bfb712a08b98
Blendar Kabashi <blendk@outlook.com>
2019-09-22 23:39:16 +0200

    Shembulli 11

--- Example11.py
+++ Example11.py
@@ -1,17 +1,36 @@
 import json
 
-def greet_user():
+def get_stored_username():
 
     filepath = 'C:\\Users\\Fitore Muqaj\\Desktop\\firstfile.json'
 
     try:
-        with open(filepath) as fileObject:
-            username = json.load(fileObject)
+        with open(filepath) as f_obj:
+            username = json.load(f_obj)
     except FileNotFoundError:
-        with open(filepath, 'w') as fileObject:
-            username = input("Shkruaj username-in tend: ")
-            json.dump(username, fileObject)
+        return None
     else:
+        return username
+
+def get_new_username():
+    """Prompt for a new username."""
+    username = input("What is your name? ")
+    filepath = 'C:\\Users\\Fitore Muqaj\\Desktop\\firstfile.json'
+    with open(filepath, 'w') as f_obj:
+        json.dump(username, f_obj)
+    return username
+
+def greet_user():
+
+    username = get_stored_username()
+    if username:
         print("Mireseerdhe prape "+username)
+    else:
+        username = get_new_username()
+        filepath = 'C:\\Users\\Fitore Muqaj\\Desktop\\firstfile.json'
+        with open(filepath,'w') as f_obj:
+            json.dump(username,f_obj)
+
+
 
 greet_user()
