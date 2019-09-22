acca3a88abb52773918fd589a29e4c70e424eb7d
Blendar Kabashi <blendk@outlook.com>
2019-09-22 02:05:54 +0200

    Add Example9.py

---
+++ SimpleExamples.py
@@ -0,0 +1,12 @@
+# py_digits.txt
+from random import randint
+
+filepath = 'C:\\Users\\Fitore Muqaj\\Desktop\\username.txt'
+string=''
+with open(filepath,'w') as file_object:
+    for i in range (0,10):
+        for j in range (0,5):
+            file_object.write(str(randint(0,9)))
+        file_object.write('\n')
+
+        
