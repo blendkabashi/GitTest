cc0ff80902dd4f806dbf1d7be6eeec2b1e0c56c2
Blendar Kabashi <blendk@outlook.com>
2019-09-23 04:09:43 +0200

    Day 1 - Python project

---
+++ data_visualization/mpl_squares.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+
+x_poi = list(range(1, 5001))
+y_poi = [x**3 for x in x_poi]
+plt.scatter(x_poi, y_poi, c=y_poi, cmap=plt.cm.Reds,
+            s=50, edgecolors='none')
+
+plt.tick_params(axis='both', labelsize=10)
+
+plt.savefig('plotFig.png', bbox_inches='tight')
